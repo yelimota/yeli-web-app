5e34166a31abeddb5b4d5f699c69b7e75e86ccf0
Osvaldo Tornes Galeana <devosvaltor@gmail.com>
2021-11-16 03:59:04 -0600

    Header was amended by placing his pug code

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,25 @@
 <template lang='pug'>
 
   header.header
-    p soy un Header
+    nav
+      button.advisor ASESORA
+      button.lawyer ABOGADA
+      .nav-desktop-button
+        button.curiosities CURIOSIDADES
+        button.schedules HORARIOS
+        button.moderators MODERADORES
+      select.languages
+        option ESPAÑOL
+        option INGLES
+      button(class="fas fa-bars button-nav")
+      button.togglebar
+        img(src="imagetogglebarLight.png" class="toggleBarLight")
+        img(src="imagetogglebarDark.png" class="toggleBarDark")
+    //- nav
+    //-   a(href="#", class="fas fa-bars")
+    //-   .togglebar
+    //-     img(src="imagetogglebarLight.png" class="toggleBarLight")
+    //-     img(src="imagetogglebarDark.png" class="toggleBarDark")
 
 </template>
 
