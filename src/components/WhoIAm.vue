98098ea31063a914bc25154121280ea150687969
Gersom Alaja <gersomalaja@gmail.com>
2021-11-16 22:40:29 -0500

    Rename Quiensoy to WhoIAm & refractoring class html

---
+++ src/components/WhoIAm.vue
@@ -0,0 +1,36 @@
+<template lang='pug'>
+
+  .who-i-am
+    .who-i-am__content
+      h1.who-i-am__title
+        |¿No sabes quien soy?
+      p.who-i-am__description
+        |Me llamo Yelitza Mota, pero me puedes llamar Yeli, y si, mi apellido real es Mota, no es broma. Y no, no vendo, <del> por ahora.</del>
+
+      .who-i-am__more-info-one
+        p.who-i-am__more-info-one__paragraph
+          |Me gusta leer, novelas de ficción que me hagan salir del mundo real, amo las conversaciones amenas entre amigos, soy muy emotiva y alegre.
+      .who-i-am__more-info-two
+        p.who-i-am__more-info-two__paragraph
+          |Aun parezco quinceañera lo se, pero en realidad tengo 26, soy abogada e streamer, mi color favorito en el azul, pero estoy poseída por el rosa, Cumplo el 5 de Noviembre ( no lo olvides eh), me gusta escuchar música, me hace sentir que soy protagonista de una historia, la cual estoy yo como espectadora, ah por supuesto me gusta la pizza, porque aja.
+
+      button.who-i-am__button-showMore Ver mas
+
+    figure.who-i-am__avatar
+      img.who-i-am__avatar__image(
+        src="" alt="Avatar Yeli")
+      figcaption.who-i-am__avatar__caption
+        |Animation By Friky
+
+</template>
+
+<script>
+
+export default {
+  name: 'WhoIAm'
+}
+
+</script>
+
+<style lang='stylus' scoped>
+</style>
