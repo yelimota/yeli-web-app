eef49ae739bba5d7d0c48b1e9ba5001414aa7beb
Osvaldo Tornes Galeana <devosvaltor@gmail.com>
2021-11-18 00:10:48 -0600

    Has been include the pug code to this archive

---
+++ src/components/SocialButton.vue
@@ -0,0 +1,17 @@
+<template lang='pug'>
+
+  button(class="btn btn-twitch cover-page__button")
+    span( class="icon") Twitch
+    span Twitch
+
+</template>
+<script>
+
+export default {
+  name: 'SocialButton'
+}
+
+</script>
+
+<style lang='stylus' scoped>
+</style>
