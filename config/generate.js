0e88e7e8382ac25f6790d1d6c2bdbd34516117d1
Gersom Alaja <gersomalaja@gmail.com>
2021-11-19 22:27:27 -0500

    config build and generate files

---
+++ config/generate.js
@@ -0,0 +1,17 @@
+export default {
+
+  generate: {
+    routes: [
+      // STATIC ROUTES
+      '/',
+      '/abogada',
+      '/asesora',
+      '/404'
+
+      // DYNAMIC ROUTES
+      // '/items/1',
+      // '/items/2',
+      // '/items/3'
+    ]
+  }
+}
