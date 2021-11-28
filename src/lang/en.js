ad50d38d35b71c1518b852f8dcc48c5487105e15
Osvaldo Tornes Galeana <devosvaltor@gmail.com>
2021-11-27 18:16:07 -0600

    Has been added the file src/lang/ and inside her has been added the documents en.js and es.js

---
+++ src/lang/en.js
@@ -0,0 +1,56 @@
+
+const EngData = {
+
+  links: ['schedules', 'moderators', 'curiosities'],
+  subtitle: ['streamer / YouTuber', 'Creator content'],
+  who_i_am: {
+    title: 'Do not you know who I am?',
+    text: ['My name is Yelitza Mota, but you can call me Yeli, and yes, my real surname is Mota, it is not a joke, and no, I do not sell, for now. ',
+      'I like to read, fiction novels that make me leave the real world, I love the conversations Avians between friends, I am very emotional and cheerful.']
+  },
+  see_more: 'I still look like I am fifteen, but I am really 26, I am a lawyer and streamer, my favorite color is blue, but I am possessed by rose, my birthday is November 5 (do not forget huh), I like it I listen to music It makes me feel that I am the protagonist of a story, that I am as a spectator, ah of course I like pizza, because uh huh.',
+
+  videoGames: {
+    title: 'Video games',
+    text: 'We had fun by playing things in stream, interacting with the chat and laughing at how you can be'
+  },
+
+  just_chatting: {
+    text_image: 'What did he say?',
+    title: 'Chatting',
+    text: ['We talked about very interesting and fun songs. ', ' Many times it costs us to believe what we have spoken']
+
+  },
+
+  freestyle: {
+    title: 'Freestyle',
+    text: 'One of the things that I pass most is the Freestyle, the ingenuity, the Flow, the fury of the moment and everything that tranmits makes it something amazing'
+  },
+
+  moderators: {
+    title: 'Moderators',
+    text: 'Friends Friends at the service of the Queen'
+  },
+
+  Gersom: {
+    title: 'Gersom',
+    text: 'A very curious boy, with surprising visions, serious but very capable'
+  },
+
+  Emmanuel: {
+    title: 'Emmanuel',
+    text: 'Musical composer and poet, dj, producer, actor, talented, comprehensive, passionate, ready, opportunistic'
+  },
+
+  Cisco: {
+    title: 'Cisco',
+    text: 'A very thoughtful person, aware of himself at the social entity, likes to make jokes, talk a lot, without fear of musical expression and poet, dj, producer, actor, talented, full, passion, ready, opportunistic'
+  },
+
+  Riny: {
+    title: 'Riny',
+    text: 'Lover of music, dancing, kitchen and coffee.I am described as solidarity, quiet, friendly and friendly, sometimes bitter'
+  }
+
+}
+export default EngData
---
+++ src/lang/es.js
@@ -0,0 +1,56 @@
+
+const EspData = {
+
+  links: ['horarios', 'moderadores', 'curiosidades'],
+  subtitle: ['streamer / YouTuber', 'Creadora de contenido'],
+  who_i_am: {
+    title: '¿No sabes quien soy yo?',
+    text: ['Me llamo Yelitza Mota, pero me puedes llamar Yeli, y sí, mi apellido real es Mota, no es broma, y no, no vendo, por ahora.',
+      'Me gusta leer, novelas de ficción que me hagan salir del mundo real, amo las conversaciones amenas entre amigos, soy muy emotiva y alegre.']
+  },
+  see_more: 'Aun parezco quinceañera lo se, pero en realidad tengo 26, soy abogada e streamer, mi color favorito en el azul, pero estoy poseída por el rosa, Cumplo el 5 de Noviembre ( no lo olvides eh), me gusta escuchar música, me hace sentir que soy protagonista de una historia, la cual estoy yo como espectadora, ah por supuesto me gusta la pizza, porque aja.',
+
+  videoGames: {
+    title: 'Video Juegos',
+    text: 'Nos divertimos jugando cosas en stream, interactuando con el chat y riendonos de lo manca que puedo ser'
+  },
+
+  just_chatting: {
+    text_image: '¿Que dijo?',
+    title: 'Charlando',
+    text: ['Conversamos sobre temas muy interesantes y divertidos.', 'Muchas veces hasta nos cuesta creer lo que hemos hablado']
+
+  },
+
+  freestyle: {
+    title: 'Freestyle',
+    text: 'Una de las cosas que mas me apasionan es el Freestyle, el ingenio, el flow, el furor del momento y todo lo que tranmite hace que sea algo asomboso'
+  },
+
+  moderators: {
+    title: 'Moderadores',
+    text: 'Amigos fieles al servicio de la reina'
+  },
+
+  Gersom: {
+    title: 'Gersom',
+    text: 'Un chico muy curioso, con unas visiones sorprendentes, serio pero muy capaz'
+  },
+
+  Emmanuel: {
+    title: 'Emmanuel',
+    text: 'Compositor musical y Poeta, Dj, productor, actor, talentoso, cabal, pasional, listo, oportunista'
+  },
+
+  Cisco: {
+    title: 'Cisco',
+    text: 'Una persona muy pensativa, consiente de si mismo en el ente social, le gusta hacer bromas, hablar mucho, sin temor a expresarseitor musical y Poeta, Dj, productor, actor, talentoso, cabal, pasional, listo, oportunista'
+  },
+
+  Riny: {
+    title: 'Riny',
+    text: 'Amante de la música, el baile, la cocina y el café. Se me describe como solidaria, tranquila, amable y amigable, a veces amargada'
+  }
+
+}
+export default EspData
